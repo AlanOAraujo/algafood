98a2a67f5d2e4a67022dc19d16d0b3fe1eaf9812
Alan Araujo <alan.araujo@unicid.edu.br>
2020-01-28 17:59:08 -0300

    Evitando ambiguidade de beans e injeção em lista de beans

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,18 +1,15 @@
 package com.algaworks.algafood.notificacao;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.model.Cliente;
 import com.algaworks.algafood.notificacao.interfase.Notificador;
 
-@Primary
 @Component
 public class NotificadorEmail implements Notificador {
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		
 		System.out.println(String.format("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem));
 	}

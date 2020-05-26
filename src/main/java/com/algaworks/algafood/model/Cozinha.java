ea123f232b8b8cc26b2bab93f7668661bf7ea8c6
Alan Araujo <alanoliveiracx@gmail.com>
2020-05-25 23:55:35 -0300

    Criação da entidades Restaurante e Cozinha

---
+++ src/main/java/com/algaworks/algafood/model/Cozinha.java
@@ -0,0 +1,47 @@
+package com.algaworks.algafood.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TAB_COZINHA")
+public class Cozinha {
+
+    @Id
+    @SequenceGenerator(sequenceName = "SEQ_TAB_COZINHA", name = "SEQ_TAB_COZINHA", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_TAB_COZINHA")
+    @Column(name = "ID_COZINHA")
+    private Long idCozinha;
+
+    @Column(name = "NM_COZINHA")
+    private String nome;
+
+    public Long getIdCozinha() {
+        return idCozinha;
+    }
+
+    public void setIdCozinha(Long idCozinha) {
+        this.idCozinha = idCozinha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return idCozinha.equals(cozinha.idCozinha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCozinha);
+    }
+}
---
+++ src/main/java/com/algaworks/algafood/model/Restaurante.java
@@ -0,0 +1,60 @@
+package com.algaworks.algafood.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TAB_RESTAURANTE")
+public class Restaurante {
+
+    @Id
+    @SequenceGenerator(sequenceName = "SEQ_TAB_RESTAURANTE", name = "SEQ_TAB_RESTAURANTE", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_TAB_RESTAURANTE")
+    @Column(name = "ID_RESTAURANTE")
+    private Long idRestaurante;
+
+    @Column(name = "NM_RESTAURANTE")
+    private String nmRestaurante;
+
+    @Column(name = "TX_FRETE")
+    private BigDecimal taxaFrete;
+
+    public Long getIdRestaurante() {
+        return idRestaurante;
+    }
+
+    public void setIdRestaurante(Long idRestaurante) {
+        this.idRestaurante = idRestaurante;
+    }
+
+    public String getNmRestaurante() {
+        return nmRestaurante;
+    }
+
+    public void setNmRestaurante(String nmRestaurante) {
+        this.nmRestaurante = nmRestaurante;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurante that = (Restaurante) o;
+        return Objects.equals(idRestaurante, that.idRestaurante);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idRestaurante);
+    }
+}
+

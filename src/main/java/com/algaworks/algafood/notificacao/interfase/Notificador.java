2e3ff43bd777793113bc2aeaf68f9ffa7953d410
Alan Araujo <alanoliveiracx@gmail.com>
2020-07-06 15:13:29 -0300

    Remoção de classes não mais utilizadas

--- src/main/java/com/algaworks/algafood/notificacao/interfase/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafood.notificacao.interfase;
-
-import com.algaworks.algafood.model.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-	
-}

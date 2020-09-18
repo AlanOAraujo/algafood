dd20e50f75d663efe0f849a6f1f9692c767a0a15
Alan Oliveira de Almeida Araujo <alanoliveiradealmeidaaraujo@iMac-de-Alan.local>
2020-09-17 21:12:17 -0300

    Removendo Target

--- target/classes/Scripts/InsertsRestaurante.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tab_restaurante (id_restaurante, nm_restaurante, taxa_frete)
-VALUES (nextval('seq_tab_restaurante'), 'Arrisca Faca', '2.20');
-
-INSERT INTO tab_restaurante (id_restaurante, nm_restaurante, taxa_frete)
-VALUES (nextval('seq_tab_restaurante'), 'Kakashi Cozinha Japonesa', '5.80');
